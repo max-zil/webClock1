0d8b03012fa26a16c7d1de546ed1a6e4f3351fdf
Max Zilberfain <max.zilberfain@getfabric.com>
2025-04-02 01:07:07 +0300

    adding all files

---
+++ public/css/script.js
@@ -0,0 +1,15 @@
+//var tst = require("./module1");
+
+// let age = prompt('How old are you?', 100);
+// alert(`You are ${age} years old!`); // You are 100 years old!
+function format(a) {
+    if (a <= 9) {
+        return "0" + a 
+    }else{
+        return a
+    }
+}
+let date = new Date();
+let timeString = date.getHours() +":" + format(date.getMinutes()) + ":" + format(date.getSeconds());
+document.getElementById("clock").innerText = timeString;
+//tst.test();
