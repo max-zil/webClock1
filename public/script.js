3846ef2944927ccb0b9ccaea1d037b970e4a217e
Max Zilberfain <max.zilberfain@getfabric.com>
2025-04-02 05:25:52 +0300

    added working timer feature that starts at 0, no stop btn

--- public/script.js
+++ public/script.js
@@ -15,3 +15,40 @@ function updateClock(){
     document.getElementById("clock").innerText = timeString;
     setTimeout(updateClock, 1000);
 }
+
+
+//add event listeners to buttons
+const element = document.getElementById("addTimerBtn");
+element.addEventListener("click", addTimer);
+
+function addTimer(){
+    let h = 0, m = 0, s = 0;
+    let timeString = h +":" + format(m) + ":" + format(s);
+    let newDiv = document.createElement("div");
+    let newLabel = document.createElement("label");
+    newLabel.id = "label1";
+    newLabel.innerText = timeString;
+    newDiv.appendChild(newLabel);
+    let add1 = document.getElementById("origin");
+    add1.appendChild(newDiv);
+    updateTimer(0);
+}
+
+function updateTimer(seconds=0, minutes=0, hours=0){
+    let h = hours, m = minutes, s = seconds;
+    if(s >= 60){
+        m++;
+        s=0;
+    }
+    else if(m >= 60){
+        h++;
+        m=0;
+    }
+    let timeString = h +":" + format(m) + ":" + format(s);
+    let timer = document.getElementById("label1");
+    timer.innerText = timeString;
+    setTimeout( function(){
+        updateTimer(s+1,m,h)}
+        ,1000);
+    
+}
