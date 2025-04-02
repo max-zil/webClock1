19e56b0fa66cc748c9fb6b88a7fd75f045d1bee8
Max Zilberfain <max.zilberfain@getfabric.com>
2025-04-02 05:58:35 +0300

    added working timer feature that starts at 0, with stop btn, resume not working

--- public/script.js
+++ public/script.js
@@ -1,7 +1,9 @@
 //var tst = require("./module1");
-
 // let age = prompt('How old are you?', 100);
 // alert(`You are ${age} years old!`); // You are 100 years old!
+/*************************************************************/
+//global vars:
+var stopped = false;
 function format(a) {
     if (a <= 9) {
         return "0" + a 
@@ -26,9 +28,15 @@ function addTimer(){
     let timeString = h +":" + format(m) + ":" + format(s);
     let newDiv = document.createElement("div");
     let newLabel = document.createElement("label");
+    //add stop Btn
+    let stopBtn = document.createElement("button");
+    stopBtn.innerText = "Stop";
+    stopBtn.id = "stopBtn";
+    stopBtn.addEventListener("click",stopTimer);
     newLabel.id = "label1";
     newLabel.innerText = timeString;
     newDiv.appendChild(newLabel);
+    newDiv.appendChild(stopBtn);
     let add1 = document.getElementById("origin");
     add1.appendChild(newDiv);
     updateTimer(0);
@@ -47,8 +55,17 @@ function updateTimer(seconds=0, minutes=0, hours=0){
     let timeString = h +":" + format(m) + ":" + format(s);
     let timer = document.getElementById("label1");
     timer.innerText = timeString;
-    setTimeout( function(){
+    const myTimeout = setTimeout( function(){
         updateTimer(s+1,m,h)}
         ,1000);
-    
+    if(stopped){
+        clearTimeout(myTimeout);
+    }
+}
+
+function stopTimer(){
+    alert("Timer Stopped");
+    stopped = true;
+    let btn1 = document.getElementById("stopBtn");
+    btn1.innerText = "Resume";    
 }
