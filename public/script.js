bdca4ef6cd3313c6cddae3520049a81ea0ffd197
Max Zilberfain <max.zilberfain@getfabric.com>
2025-04-11 00:25:46 +0300

    cleaned up code

--- public/script.js
+++ public/script.js
@@ -1,7 +1,3 @@
-//var tst = require("./module1");
-// let age = prompt('How old are you?', 100);
-// alert(`You are ${age} years old!`); // You are 100 years old!
-/*************************************************************/
 
 function format(a) {
     if (a <= 9) {
@@ -10,10 +6,12 @@ function format(a) {
         return a
     }
 }
+
 function timeToString(elem){
     let timeString = elem.getHours() +":" + format(elem.getMinutes()) + ":" + format(elem.getSeconds());
     return timeString;
 }
+
 function updateClock(){
     let date = new Date();
     let timeString = timeToString(date);
@@ -28,20 +26,21 @@ element.addEventListener("click", addTimer);
 //global labelID - Counter of number of created timers
 let labelID = 0;
 function addTimer(){
-    //let labelID = 0; #this would be a local variable that is initialized each time
-    //a new timer is added to 0 and then incremented to 1 for each new timer,
-    //a global version of the labelID (outside the function addTimer) will work correctly.
+    //new timer - increment timer counter
     labelID++;
     let myTimeout;
     let h = 0, m = 0, s = 0;
-    var stopped = false;
+    let stopped = false;
+
     let timeString = h +":" + format(m) + ":" + format(s);
     let newDiv = document.createElement("div");
     let newLabel = document.createElement("label");
-    //add stop Btn
+
     let stopBtn = document.createElement("button");
     stopBtn.innerText = "Stop";
     stopBtn.id = "stopBtn" + labelID;
+
+
     stopBtn.addEventListener("click",function(){
         alert("Timer Stopped");
         stopped = true;
@@ -50,14 +49,16 @@ function addTimer(){
         btn1.style.display = 'none';
         clearTimeout(myTimeout);
     });
+    //###Label Setup
     //select the label id of the correct and current instance of the timer 
     let labelString = "label" + labelID;
     newLabel.id = labelString;
     newLabel.innerText = timeString;
+    //###Append elements
     newDiv.appendChild(newLabel);
     newDiv.appendChild(stopBtn);
-    let add1 = document.getElementById("origin");
-    add1.appendChild(newDiv);
+    document.getElementById("origin").appendChild(newDiv);
+    
     function updateTimer(){
         if(stopped){
             return;
@@ -73,8 +74,7 @@ function addTimer(){
         }
         let timeString = h +":" + format(m) + ":" + format(s);
         
-        let timer = document.getElementById(labelString);
-        timer.innerText = timeString;
+        document.getElementById(labelString).innerText = timeString;
         myTimeout = setTimeout(updateTimer,1000);
     }
     updateTimer(0);
